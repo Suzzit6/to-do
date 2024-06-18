01e548e46d5c9f4cb1a36b8d104cb84bf490816a
sujit mishra <mishrasujit409@gmail.com>
2024-06-18 19:19:32 +0530

    new

---
+++ src/contexts/ToDocontext.js
@@ -0,0 +1,21 @@
+import { useContext,createContext } from "react";
+
+export const ToDoContext = createContext({
+    todos:[
+        {
+            id:1,
+            todo:" msg ",
+            completed:false
+        }
+    ],
+    addTodo: (todo) => {},
+    updateTodo: (id,todo) => {},
+    deleteTodo: (id) => {},
+    toggleComplete: (id) => {}
+})
+
+export  const useToDo = ()=>{
+    return useContext(ToDoContext)
+}
+
+export const ToDoProvider = ToDoContext.Provider
